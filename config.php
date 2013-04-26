1a6adeef2f4338c464ef0a158a02a48b6857d04c
Hans Kokx <skipmeister123@gmail.com>
2013-04-26 14:20:22 -0400

    wiping index

---
+++ config.php
@@ -0,0 +1,11 @@
+<?
+
+// database stuff
+require_once 'idiorm.php';
+ORM::configure(array(
+    'connection_string' => 'pgsql:host=localhost;dbname=hanskokx',
+    'username' => 'dev',
+    'password' => 'top_secret'
+));
+
+?>
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-<?php include("title.php"); ?>
-<!-- Begin services -->
-	<div id="services" class="tagline">
-		<div class="title user-select-none cursor_default">Candid portraits of real moments.</div>
-	</div>
-<!-- End services -->
-<?php include("footer.php"); ?>
+<?
+require_once 'config.php';
+
+echo "hello";
+
+?>
