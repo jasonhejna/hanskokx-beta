5f48a1f166ce6d83566eb2d70c1b3cd54022e58e
Hans Kokx <skipmeister123@gmail.com>
2013-04-26 14:58:58 -0400

    get record back

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 require_once 'config.php';
 
 $new = ORM::for_table('clients')->create();
-
 $new->set('name', 'Bob');
 $new->email = 'test@test.com';
-
 $new->save();
 
+
+$person = ORM::for_table('clients')->where('name', 'Bob')->find_one();
 
 ?>
