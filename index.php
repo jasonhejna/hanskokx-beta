e33033f064325879e1b17652f2913e95fdfc395e
Hans Kokx <skipmeister123@gmail.com>
2013-04-27 13:07:21 -0400

    testing functional call

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?
-require_once 'config.php';
+# Set up the session
+session_start();
 
-echo $mobile;
+# Global configuration
+require_once 'config.php';
+?>
+
+<?
+curPageName();
+?>
+
+<!-- Begin services -->
+	<div id="services" class="tagline">
+		<div class="title user-select-none cursor_default">Candid portraits of real moments.</div>
+	</div>
+<!-- End services -->
+
+<?
+include("footer.php");
 ?>
