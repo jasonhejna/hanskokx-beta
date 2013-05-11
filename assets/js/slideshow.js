bcb17edf89e185aaa8b329b938f460e6f3a131eb
Hans Kokx <skipmeister123@gmail.com>
2013-05-11 18:15:43 -0400

    trying to fix slideshow.js

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -20,7 +20,7 @@ $(window).load(function(){
 		window.console && window.console.time && console.time('Generated In');
 		
 		if(supportCanvas){
-			$('#slideshow img').each(function(){
+			$('#slideshow li').each(function(){
 
 				if(!slideshow.width){
 					// Taking the dimensions of the first image:
